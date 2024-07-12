c5d45bd0ddd097a665c7bac3c32a025bc620264d
ryuyeonkyoung <ykr.dev@gmail.com>
2024-07-12 12:29:48 +0900

    7/10(수) 공부한 곳까지

---
+++ src/main/java/com/example/crud_practice/CrudPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPracticeProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/crud_practice/controller/BoardController.java
@@ -0,0 +1,26 @@
+package com.example.crud_practice.controller;
+
+import com.example.crud_practice.dto.BoardDTO;
+import com.example.crud_practice.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        //boardService.save(boardDTO);
+        return "index";
+    }
+}
