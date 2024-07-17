d63b94b7da1da7d28102355513239e6f2cac2359
ryuyeonkyoung <ykr.dev@gmail.com>
2024-07-17 17:32:09 +0900

    05_게시글 조회

--- src/main/java/com/example/crud_practice/repository/BoardRepository.java
+++ src/main/java/com/example/crud_practice/repository/BoardRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     // update board_table set board_hits=board_hits+1 where id=?
-    @Modifying
+    @Modifying //update나 delete 쿼리를 실행할 때 필수로 붙이기
     @Query(value = "update BoardEntity b set b.boardHits=b.boardHits+1 where b.id=:id")
     void updateHits(@Param("id") Long id);
 }
