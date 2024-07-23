7432bec46adb83ea20573ae18f7f604f6146a714
ryuyeonkyoung <ykr.dev@gmail.com>
2024-07-23 13:33:05 +0900

    09_파일첨부_첨부파일 요청보내기

--- src/main/java/com/example/crud_practice/dto/BoardDTO.java
+++ src/main/java/com/example/crud_practice/dto/BoardDTO.java
@@ -3,6 +3,7 @@ package com.example.crud_practice.dto;
 import com.example.crud_practice.entity.BaseEntity;
 import com.example.crud_practice.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,12 @@ public class BoardDTO {
     private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+
+    private MultipartFile boardFile; // save.html -> Controller 파일 담는 용도
+    private String originalFileName; // 원본 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
+    //파일 담는 용도
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
