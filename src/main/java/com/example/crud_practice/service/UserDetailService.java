6734f433a119dea0ec81f8ecf13c71b0b88d6854
류연경 <ykr.dev@gmail.com>
2025-03-29 17:22:16 +0900

    feat: Spring Security 기반 로그인/회원가입 기능 및 인증/인가 설정 적용
    - 로그인/회원가입 Form 구성 및 처리 로직 구현
    - 인증 설정: UserDetailService, PasswordEncoder, AuthenticationManager
    - 인가 설정: 로그인하지 않은 사용자는 모든 요청 차단

---
+++ src/main/java/com/example/crud_practice/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.example.crud_practice.service;
+
+import lombok.RequiredArgsConstructor;
+import com.example.crud_practice.entity.User;
+import com.example.crud_practice.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                 .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
